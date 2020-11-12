31e981af1c94172b642a24b822f57ca90def65ae
yutanglidemao <richardyangcz@163.com>
2020-11-12 17:42:20 +0800

    Initial commit

---
+++ src/main/java/richard/yang/personaleurekaserver/PersonalEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package richard.yang.personaleurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalEurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalEurekaServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/richard/yang/personaleurekaserver/PersonalEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package richard.yang.personaleurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
