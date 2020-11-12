285fea6adf437f5931b7993401e72dc423b8f638
yutanglidemao <richardyangcz@163.com>
2020-11-12 18:04:47 +0800

    添加build.sh

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+
+mvn clean package -Dmaven.test.skip=true -U
+
+
--- src/main/java/richard/yang/personaleurekaserver/PersonalEurekaServerApplication.java
+++ src/main/java/richard/yang/personaleurekaserver/PersonalEurekaServerApplication.java
@@ -2,8 +2,10 @@ package richard.yang.personaleurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class PersonalEurekaServerApplication {
 
     public static void main(String[] args) {
